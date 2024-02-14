fc4e472316c935ba00653d30aa8dd4e8dec8c3b5
Backiaraj <backiaraj.kanagaraj@syncfusion.com>
2024-02-14 16:27:51 +0530

    Update the project

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,27 +1,13 @@
+using BlazorScheduler.Client;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 using Syncfusion.Blazor;
-using System;
-using System.Net.Http;
-using System.Threading.Tasks;
 
-namespace BlazorScheduler.Client
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            builder.RootComponents.Add<App>("#app");
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSyncfusionBlazor();
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-            builder.Services.AddSyncfusionBlazor();
-
-            await builder.Build().RunAsync();
-        }
-
-    }
-
-}
+await builder.Build().RunAsync();
